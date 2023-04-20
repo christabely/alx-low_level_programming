127db2c2ae82431acb662d02f90aba40a09d3793
christabel <christabelaidoo71@gmail.com>
2023-04-20 06:58:08 -0700

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
-int main() {
-    puts("Programming is like building a multilingual puzzle");
+/**
+*main - main - entry point
+*/
+init main(void)
+{
+    puts("/"Programming is like building a multilingual puzzle");
     return 0;
 }
 
