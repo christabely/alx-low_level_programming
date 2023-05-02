b00a0eb92b383fb1a9c7c5d3a9eb652e116cdccb
christabel <christabelaidoo71@gmail.com>
2023-05-02 14:42:12 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -6,12 +6,12 @@
  */
 void _puts(char *str)
 {
-	int i = 0;
+	int u = 0;
 
-	while (*(str + i) != '\0')
+	while (*(str + u) != '\0')
 	{
-		_putchar(*(str + i));
-		i++;
+		_putchar(*(str + u));
+		u++;
 	}
 	_putchar('\n');
 }
