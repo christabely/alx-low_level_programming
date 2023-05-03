f327f9941d48b3b6c4b7027f7c1fc2792beaaf5a
christabel <christabelaidoo71@gmail.com>
2023-05-03 06:49:09 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,23 +2,20 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - generates passwds
+ * main - generate passwd
  * Return: void
  */
 int main(void)
 {
 	int r = 0, u = 0;
 
-	time_t t;
-
-	srand((unsigned int) time(&t));
 	while (u < 2772)
 	{
-		r = rand() % 128;
-		if ((u + r) > 2772)
-			break;
-		u = u + r;
-		printf("%u", r);
+	r = rand() % 128;
+	if ((u + r) > 2772)
+	break;
+	u = u + r;
+	printf("%u", r);
 	}
 	printf("%u\n", (2772 - u));
 	return (0);
