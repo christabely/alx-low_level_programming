1b881df4ed1229383d699fb5ff8921ebd636447f
Christabel <christabelaidoo71@gmail.com>
2023-07-26 21:11:21 -0700

    dynamiclybraries

---
+++ 0x18-dynamic_libraries/README.md
@@ -0,0 +1,1 @@
+project_0x18-dynamic_libraries
---
+++ 0x18-dynamic_libraries/_isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isdigit - checks for numerals
+ * @c: input
+ * Return: 1 if is digit, else 0
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
