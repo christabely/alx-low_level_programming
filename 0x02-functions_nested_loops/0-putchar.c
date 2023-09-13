378b16809fec0567339491f9956560e6e37617dc
Christabel <christabelaidoo71@gmail.com>
2023-09-12 22:06:26 -0700

    updated today

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*main - Entry point of fnction
+*Return: 0 when successful
+*/
+int main(void)
+{
+	char s[] = "_putchar";
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	_putchar(10);
+
+	return (0);
+}
